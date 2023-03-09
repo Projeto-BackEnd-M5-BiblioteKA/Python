719d20dc3954579135c92851ad401c29691e93ec
Gabriel Malafaia <gabrielbsnsp@gmail.com>
2023-03-09 15:19:10 -0300

    feat: User queries for Admin per id (get, patch, delete)

--- users/views.py
+++ users/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
 from .models import User
 from .serializers import UserSerializer
-from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 
 
-class UserView(CreateAPIView):
-    queryset = User
+class UserView(ListCreateAPIView):
+    queryset = User.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_superuser:
+            return User.objects.all()
+
+        return User.objects.filter(id=user.id)
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [IsAuthenticated()]
+
+
+class UserViewDetail(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
+
+    def perform_destroy(self, instance):
+        instance.is_blocked = True
+        instance.save()
