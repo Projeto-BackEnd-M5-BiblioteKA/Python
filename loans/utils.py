ad5a0fca83e459371da0ef1093eb76631cf01598
Gabriel Malafaia <gabrielbsnsp@gmail.com>
2023-03-14 17:53:41 -0300

    fix: files in loans

--- loans/utils.py
+++ loans/utils.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 
 
 def default_devolution_date():
-    devolution_date = date.today() + timedelta(days=-3)
+    devolution_date = date.today() + timedelta(days=7)
 
     if devolution_date.weekday() == 5:
         devolution_date = devolution_date + timedelta(days=2)
